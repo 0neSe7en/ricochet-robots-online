25719af22693a35bcfb365ba299e95af836e472d
0neSe7en <siyuan.se7en@gmail.com>
2015-08-03 10:54:51 +0800

    get target

--- js/utils.js
+++ js/utils.js
@@ -6,3 +6,23 @@ function toBoard(x, y) {
     y: options.board.offY + y * options.board.gridSize
   }
 }
+
+function getTarget(robot, direction) {
+  var operations = [{x: 0, y: -1}, {x: 1, y: 0}, {x: 0, y: 1}, {x: -1, y: 0}];
+  var currentPos = {x: robot.x, y: robot.y};
+  while (currentPos.x < options.board.count - 1 &&
+         currentPos.y < options.board.count - 1) {
+    if (board[currentPos.x][currentPos.y].direction[direction]) {
+      if (board[currentPos.x + operations[direction].x]
+               [currentPos.y + operations[direction].y].available) {
+        currentPos.x += operations[direction].x;
+        currentPos.y += operations[direction].y;
+      } else {
+        break;
+      }
+    } else {
+      break;
+    }
+  }
+  return currentPos;
+}
